2f36b02b755a9eb378548f61baa2863f00663168
lahemotlagh <136149631+lahemotlagh@users.noreply.github.com>
2026-05-01 23:11:57 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,23 @@ import requests
 import gspread
 from google.oauth2.service_account import Credentials
 
+import os
+import threading
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def health_check():
+    return "Bot is running!", 200
+
+def run_flask():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+threading.Thread(target=run_flask, daemon=True).start()
+
+
 # --- تنظیمات اولیه ---
 TOKEN = os.environ.get('BALE_TOKEN')
 # خواندن JSON از محیط Render
